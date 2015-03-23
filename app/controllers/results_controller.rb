c7114f704dfc4c8ade5fc3ef8fb6fe49303c6ce6
rameshabhat <ramya.bhat88@gmail.com>
2015-03-22 20:42:04 -0400

    Update results_controller.rb
    Add Year and Year&Semester methods

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -13,55 +13,80 @@ class ResultsController < ApplicationController
     render "/results/answers"
   end
   
-  def abetReport
-    @slos = Answer.joins(knowledge_topic: :student_learning_outcomes).select( 'student_learning_outcomes.title', :is_correct, "count(answers.id) AS total_answers").group('student_learning_outcomes.id', :is_correct)
-    @slos_combined = Result.combine_table_lines(@slos)
+  
+  
+  def abetReportYear
     
+   @slos = Answer.joins(knowledge_topic: :student_learning_outcomes).select("student_learning_outcomes.title","count(*) as total_answers","count(case when is_correct = 't' then 1 else null) as correct_answers","count(case when is_correct = 'f' then 1 else null) as incorrect_answers").where("sections.year_offered = (params[:Year])").group("student_learning_outcomes.id")
+   @slos_combined = Result.combine_table_lines(@slos)
     
-    @slos_fall = @slos.joins(:section).select( 'student_learning_outcomes.title', :is_correct, "count(answers.id) AS total_answers").where("sections.semester = 'Fall' AND sections.year_offered = 2014" ).group('student_learning_outcomes.id', :is_correct)
-    @slos_spring = @slos.joins(:section).select( 'student_learning_outcomes.title', :is_correct, "count(answers.id) AS total_answers").where("sections.semester = 'Spring' AND sections.year_offered = 2014" ).group('student_learning_outcomes.id', :is_correct)
-    @slos_summer = @slos.joins(:section).select( 'student_learning_outcomes.title', :is_correct, "count(answers.id) AS total_answers").where("sections.semester = 'Summer' AND sections.year_offered = 2014" ).group('student_learning_outcomes.id', :is_correct)
+    @slos_fall = @slos.joins(:section).select( 'student_learning_outcomes.title', :is_correct, "count(answers.id) AS total_answers").where("sections.semester = 'Fall' AND sections.year_offered = (params[:Year])" ).group('student_learning_outcomes.id', :is_correct)
+    @slos_spring = @slos.joins(:section).select( 'student_learning_outcomes.title', :is_correct, "count(answers.id) AS total_answers").where("sections.semester = 'Spring' AND sections.year_offered = (params[:Year])" ).group('student_learning_outcomes.id', :is_correct)
+    @slos_summer = @slos.joins(:section).select( 'student_learning_outcomes.title', :is_correct, "count(answers.id) AS total_answers").where("sections.semester = 'Summer' AND sections.year_offered = (params[:Year])" ).group('student_learning_outcomes.id', :is_correct)
     @slos_combined_semesters = Result.combine_table_lines_with_semesters(@slos_fall, @slos_spring, @slos_summer)
     @slos_percentages = Result.make_percentages(@slos_combined_semesters)
     
-    
-    @pathway_1160 = Section.select(:student_id, :course).distinct.where(course: ['CS 1160', 'CS 1161'])
+    @pathway_1160 = Section.select(:student_id, :course).distinct.where("course: ['CS 1160', 'CS 1161'] AND year_offered = (params[:Year])")
     @ary_1160_students = Result.getStudentIDArray(@pathway_1160)
-    @pathway_1180 = Section.select(:student_id).distinct.where.not(student_id: @ary_1160_students)
+    @pathway_1180 = Section.select(:student_id).distinct.where.not("student_id: @ary_1160_students AND year_offered = (params[:Year])")
     @ary_1180_students = Result.getStudentIDArray(@pathway_1180)
     
+    @grades_1160 = Section.select(:course, :grade, "count(*) As total_students").where(student_id: @ary_1160_students).where.not(grade: nil).group(:course, :grade)
+    @grades_1180 = Section.select(:course, :grade, "count(*) As total_students").where(student_id: @ary_1180_students).where.not(grade: nil).group(:course, :grade)
+    
+  
+    @grades_combined_1160 = Result.combine_letter_grades(@grades_1160)
+    @grades_combined_1180 = Result.combine_letter_grades(@grades_1180)
+    @grades_for_graph = Result.combine_grades_to_graph(@grades_combined_1160,@grades_combined_1180)
+    
+    @kts = Answer.joins(:knowledge_topic, :Section).select( 'knowledge_topics.knowledge_topic AS title', :is_correct, "count(answers.id) AS total_answers").where("sections.year_offered = (params[:Year])").group(:knowledge_topic, :is_correct)
+    @kts_combined = Result.combine_table_lines(@kts)
+    
+    @questions = Answer.joins(:question, :Section).select( 'question_text AS title', :is_correct, "count(answers.id) AS total_answers").where("sections.year_offered = (params[:Year])").group(:question_text, :is_correct)
+    @questions_combined = Result.combine_table_lines(@questions)
+  
+    render "/results/reportABETY"
+  
+  end
+  
+  def abetReportYearSemester
+    
+    
+    @slos = Answer.joins(knowledge_topic: :student_learning_outcomes).select("student_learning_outcomes.title","count(*) as total_answers","count(case when is_correct = 't' then 1 else null) as correct_answers","count(case when is_correct = 'f' then 1 else null) as incorrect_answers").where("sections.semester = (params[:Semester]) AND sections.year_offered = (params[:Year])" ).group("student_learning_outcomes.id")
+    @slos_combined = Result.combine_table_lines(@slos)
+    
+    #@slos_semester = @slos.joins(:section).select( 'student_learning_outcomes.title', :is_correct, "count(answers.id) AS total_answers").where("sections.semester = (params[:Semester]) AND sections.year_offered = (params[:Year])" ).group('student_learning_outcomes.id', :is_correct)
+    #@slos_combined_semesters = Result.combine_table_lines_with_semesters(@slos_fall, @slos_spring, @slos_summer)
+    
+    @slos_percentages = Result.make_percentages_per_semester(@slos_combined)
+    
+  
+    @pathway_1160 = Section.select(:student_id, :course).distinct.where("course: ['CS 1160', 'CS 1161'] AND year_offered = :year AND semester = :semester", {year: params[:year]}, {semester: params[:semester]})
+    @ary_1160_students = Result.getStudentIDArray(@pathway_1160)
+    @pathway_1180 = Section.select(:student_id).distinct.where.not("student_id: @ary_1160_students")
+    @ary_1180_students = Result.getStudentIDArray(@pathway_1180)
+  
     @grades_1160 = Section.select(:course, :grade, "count(*) As total_students").where(student_id: @ary_1160_students).where.not(grade: nil).group(:course, :grade)
     @grades_1180 = Section.select(:course, :grade, "count(*) As total_students").where(student_id: @ary_1180_students).where.not(grade: nil).group(:course, :grade)
     
     @grades_combined_1160 = Result.combine_letter_grades(@grades_1160)
     @grades_combined_1180 = Result.combine_letter_grades(@grades_1180)
     @grades_for_graph = Result.combine_grades_to_graph(@grades_combined_1160,@grades_combined_1180)
-    @retention_for_graphing = Result.number_grades_per_class(@grades_combined_1160,@grades_combined_1180)
     
-    @kts = Answer.joins(:knowledge_topic).select( 'knowledge_topics.knowledge_topic AS title', :is_correct, "count(answers.id) AS total_answers").group(:knowledge_topic, :is_correct)
+    @kts = Answer.joins(:knowledge_topic, :Section).select( 'knowledge_topics.knowledge_topic AS title', :is_correct, "count(answers.id) AS total_answers").where("sections.year_offered = (params[:Year]) AND sections.semester = (params[:Semester]").group(:knowledge_topic, :is_correct)
     @kts_combined = Result.combine_table_lines(@kts)
     
-    @questions = Answer.joins(:question).select( 'question_text AS title', :is_correct, "count(answers.id) AS total_answers").group(:question_text, :is_correct)
+    @questions = Answer.joins(:question, :Section).select( 'question_text AS title', :is_correct, "count(answers.id) AS total_answers").where("sections.year_offered = (params[:Year]) AND sections.semester = (params[:Semester]").group(:question_text, :is_correct)
     @questions_combined = Result.combine_table_lines(@questions)
-    
-    @pathway_2570 = Section.select(:student_id, :course).distinct.where(course: 'MTH 2570')
-    @ary_2570_students = Result.getStudentIDArray(@pathway_2570)
-    @pathway_2200 = Section.select(:student_id).distinct.where(course: ['CS 2200', 'CS 1200'])
-    @ary_2200_students = Result.getStudentIDArray(@pathway_2200)
-    
-    @grades_2570 = Section.select(:course, :grade, "count(*) As total_students").where(student_id: @ary_2570_students).where.not(grade: nil).group(:course, :grade)
-    @grades_2200 = Section.select(:course, :grade, "count(*) As total_students").where(student_id: @ary_2200_students).where.not(grade: nil).group(:course, :grade)
-    
-    @grades_combined_2570 = Result.combine_letter_grades(@grades_2570)
-    @grades_combined_2200 = Result.combine_letter_grades(@grades_2200)
-    @grades_for_graph_discrete = Result.combine_grades_to_graph(@grades_combined_2570,@grades_combined_2200)
-    @retention_for_graphing_discrete = Result.number_grades_per_class(@grades_combined_2570,@grades_combined_2200)
-    
-    
-    render "/results/reportABET"
+  
+    render "/results/reportABETYS"
+  
   end
-  
-  
+    
+    
 end
+
+
+
 
 
